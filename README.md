59387f3891fcbe55c59b25cf19d53840119af966
riizlaah <100104632+riizlaah@users.noreply.github.com>
2026-03-10 21:00:56 +0700

    docs: update README.md

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Enter School
 
-Manajamen antrean untuk pendaftaran sekolah
+Manajamen antrean untuk pendaftaran sekolah.
+
+## ERD
+![DB Schema](schemas/relationship.png)
+
+## Features
+- Normal User :
+  - [x] Tanpa login, bisa langsung pakai app.
+  - [x] Ambil Antrean (`Queue`) dengan nomor telepon (`Phone Number`).
+  - [x] Lihat semua `Queue` yang telah diambil suatu Perangkat (`Device` ) serta `Phone Number` nya.
+  - [ ] Lihat Detail suatu `Queue`.
+- Admin
+  - [ ] CRUD `Queue`.
+  - [ ] Menjalankan suatu `Queue`.
+  - [ ] Memanggil user yang mendaftar suatu `Queue`, pemanggilan bisa tidak berurutan (misal, pemanggilannya: no 1, 2, 5, 8, 3, 9, 6, ...).
+  - [ ] Menyelesaikan suatu `Queue` (jika semua user telah dipanggil).
+
+## Extra Features
+- [x] Sebuah `Device` dapat menyimpan banyak `Phone Number` berdasarkan `uuid` pada perangkat user yang digenerate dengan `FingerprintJS`.
--- schemas/enter_school.sql
+++ schemas/enter_school.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 09 Mar 2026 pada 13.30
--- Versi server: 10.11.13-MariaDB-0ubuntu0.24.04.1
--- Versi PHP: 8.3.6
+-- Generation Time: Mar 10, 2026 at 01:24 PM
+-- Server version: 10.11.13-MariaDB-0ubuntu0.24.04.1
+-- PHP Version: 8.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,7 +26,7 @@ USE `enter_school`;
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `devices`
+-- Table structure for table `devices`
 --
 
 CREATE TABLE `devices` (
@@ -36,7 +36,7 @@ CREATE TABLE `devices` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `devices`
+-- Dumping data for table `devices`
 --
 
 INSERT INTO `devices` (`id`, `uuid`, `alias_name`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `devices` (`id`, `uuid`, `alias_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `phone_numbers`
+-- Table structure for table `phone_numbers`
 --
 
 CREATE TABLE `phone_numbers` (
@@ -55,16 +55,20 @@ CREATE TABLE `phone_numbers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `phone_numbers`
+-- Dumping data for table `phone_numbers`
 --
 
 INSERT INTO `phone_numbers` (`id`, `device_id`, `phone_number`) VALUES
-(1, 2, '087676875564');
+(1, 2, '087676875564'),
+(2, 2, '086755476539'),
+(3, 2, '0846789965576'),
+(4, 2, '085899076003'),
+(5, 2, '085988434402');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `queues`
+-- Table structure for table `queues`
 --
 
 CREATE TABLE `queues` (
@@ -77,7 +81,7 @@ CREATE TABLE `queues` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `queues`
+-- Dumping data for table `queues`
 --
 
 INSERT INTO `queues` (`id`, `title`, `description`, `date`, `quota`, `status`) VALUES
@@ -87,7 +91,7 @@ INSERT INTO `queues` (`id`, `title`, `description`, `date`, `quota`, `status`) V
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user_queues`
+-- Table structure for table `user_queues`
 --
 
 CREATE TABLE `user_queues` (
@@ -102,25 +106,29 @@ CREATE TABLE `user_queues` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `user_queues`
+-- Dumping data for table `user_queues`
 --
 
 INSERT INTO `user_queues` (`id`, `code`, `phone_id`, `device_id`, `queue_id`, `created_at`, `called_at`, `completed_at`) VALUES
-(1, '4BCD-3FGH', 1, NULL, 1, '2026-03-09 10:04:18', NULL, NULL);
+(1, '4BCD-3FGH', 1, NULL, 1, '2026-04-09 10:04:18', NULL, NULL),
+(2, 'B3PA-XBX5', 2, 2, 1, '2026-04-09 14:23:06', NULL, NULL),
+(3, '2DJW-DU1M', 3, 2, 1, '2026-04-09 14:23:17', NULL, NULL),
+(4, 'C10E-UIHF', 4, 2, 1, '2026-04-09 14:23:40', NULL, NULL),
+(5, '4Y5D-2LLE', 5, 2, 1, '2026-04-09 14:24:07', NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `devices`
+-- Indexes for table `devices`
 --
 ALTER TABLE `devices`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `uuid` (`uuid`);
 
 --
--- Indeks untuk tabel `phone_numbers`
+-- Indexes for table `phone_numbers`
 --
 ALTER TABLE `phone_numbers`
   ADD PRIMARY KEY (`id`),
@@ -128,13 +136,13 @@ ALTER TABLE `phone_numbers`
   ADD KEY `device` (`device_id`);
 
 --
--- Indeks untuk tabel `queues`
+-- Indexes for table `queues`
 --
 ALTER TABLE `queues`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `user_queues`
+-- Indexes for table `user_queues`
 --
 ALTER TABLE `user_queues`
   ADD PRIMARY KEY (`id`),
@@ -144,45 +152,45 @@ ALTER TABLE `user_queues`
   ADD KEY `device2` (`device_id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `devices`
+-- AUTO_INCREMENT for table `devices`
 --
 ALTER TABLE `devices`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `phone_numbers`
+-- AUTO_INCREMENT for table `phone_numbers`
 --
 ALTER TABLE `phone_numbers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `queues`
+-- AUTO_INCREMENT for table `queues`
 --
 ALTER TABLE `queues`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `user_queues`
+-- AUTO_INCREMENT for table `user_queues`
 --
 ALTER TABLE `user_queues`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `phone_numbers`
+-- Constraints for table `phone_numbers`
 --
 ALTER TABLE `phone_numbers`
   ADD CONSTRAINT `device` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`) ON DELETE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `user_queues`
+-- Constraints for table `user_queues`
 --
 ALTER TABLE `user_queues`
   ADD CONSTRAINT `device2` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`) ON DELETE SET NULL,
