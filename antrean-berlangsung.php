09fbdbb1b38d77b1f72c8e8dd6f42bfa2269f361
Maull8 <muhammadmaulana248191@student.smkn1kandeman.sch.id>
2026-05-11 14:51:22 +0700

    penambahan html

--- antrean-berlangsung.php
+++ antrean-berlangsung.php
@@ -13,6 +13,27 @@ require_once "app/core.php";
   <title>Document</title>
 </head>
 <body>
-  
+  <div class="spanduk">
+    <p class="judul">Pengambilan Antrean Pendaftaran Sekolah</p>
+  </div>
+  <div class="container">
+    <!-- Sidebar -->
+    <aside class="sidebar">
+      <div class="logo">
+        <img src="/assets/img/logo-teks.png" alt="" srcset="" />
+      </div>
+      <nav class="layer-menu">
+        <ul class="menu">
+          <li><a href="/" class="btn light">Ambil Antrean</a></li>
+          <li>
+            <a href="/antrean-berlangsung.php" class="btn light">Antrean Berlangsung</a>
+          </li>
+          <li>
+            <a href="/antrean-saya.php" class="btn dark">Lihat AntreanMu</a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  </div>
 </body>
 </html>
